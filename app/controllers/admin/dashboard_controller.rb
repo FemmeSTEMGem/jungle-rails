e7a135f656120daae004b8cfb8b22c963028e692
FemmeSTEMGem <femmestemgem@gmail.com>
2022-03-09 22:07:42 -0500

    Completing 'Feature: Admin Dashboard - Counts' and 'Feature: About Page'

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
 
   include HttpAuthConcern
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
